1547492617a952ad33c5f150e48ff1227456f446
Renato Luiz de Almeida <38532053+Renato9889@users.noreply.github.com>
2022-04-06 23:48:40 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -4,6 +4,22 @@ def jogar():
     print("Bem vindo ao jogo de Forca!")
     print("*********************************")
 
+    palavraa_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra?")
+        chute = chute.strip()
+        index = 1
+
+        for letra in palavraa_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra na {} na posição {}".format(letra,index))
+            index = index+1
+        print("Jogando...")
+
     print("Fim do jogo")
 
 if(__name__=="__main__"):
